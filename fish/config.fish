747a9d4f7ed4dcc7e9bfd5aaa9344fbffeff035e
Diego Ledesma <diegoledesma0303@gmail.com>
2025-10-08 09:38:51 +0100

    dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+set -U fish_greeting " " 
+fastfetch
+
+# Created by `pipx` on 2025-10-08 07:48:54
+set PATH $PATH /home/diegoledesma/.local/bin
---
+++ hypr/scripts/random_wallpaper.sh
@@ -0,0 +1,2 @@
+sleep 2 && notify-send "Fondos de pantalla automáticos: activado"
+while true; do sleep 1800; waypaper --random; done
